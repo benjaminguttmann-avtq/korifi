0947e1813e3331cc8be61055b8cb39a417e8e30e
BG <bguttmann@anynines.com>
2025-12-05 11:58:10 +0100

    4246-stub-org-quotas

--- api/handlers/org_quota.go
+++ api/handlers/org_quota.go
@@ -1,28 +1,18 @@
 package handlers
 
 import (
-	"context"
-	"crypto/x509"
-	"encoding/pem"
-	"fmt"
 	"net/http"
 	"net/url"
-	"time"
 
-	"github.com/go-logr/logr"
-
-	"code.cloudfoundry.org/korifi/api/authorization"
-	apierrors "code.cloudfoundry.org/korifi/api/errors"
-	"code.cloudfoundry.org/korifi/api/payloads"
 	"code.cloudfoundry.org/korifi/api/presenter"
-	"code.cloudfoundry.org/korifi/api/repositories"
 	"code.cloudfoundry.org/korifi/api/routing"
-	"code.cloudfoundry.org/korifi/api/tools/singleton"
 )
 
 const (
-	OrgQuotaPath = " /v3/organization_quotas/${guid}"
+	OrgQuotaPath = "/v3/organization_quotas/${guid}"
 )
+
+type CFOrgQuotaRepository interface{}
 
 type OrgQuota struct {
 	apiBaseURL   url.URL
@@ -44,4 +34,8 @@ func (h *OrgQuota) AuthenticatedRoutes() []routing.Route {
 	return []routing.Route{
 		{Method: "GET", Pattern: OrgQuotaPath, Handler: h.get},
 	}
+}
+
+func (h *OrgQuota) UnauthenticatedRoutes() []routing.Route {
+	return nil
 }
--- api/repositories/org_quota_repository.go
+++ api/repositories/org_quota_repository.go
@@ -1,29 +1,12 @@
 package repositories
 
-import (
-	"context"
-	"fmt"
-	"maps"
-	"slices"
-	"time"
-
-	"code.cloudfoundry.org/korifi/api/authorization"
-	apierrors "code.cloudfoundry.org/korifi/api/errors"
-	korifiv1alpha1 "code.cloudfoundry.org/korifi/controllers/api/v1alpha1"
-	"code.cloudfoundry.org/korifi/tools"
-
-	"github.com/BooleanCat/go-functional/v2/it"
-	"github.com/google/uuid"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 type OrgQuotaRepo struct {
 	klient Klient
 }
 
 func NewOrgQuotaRepo(
 	klient Klient,
-) *OrgRepo {
+) *OrgQuotaRepo {
 	return &OrgQuotaRepo{
 		klient: klient,
 	}
